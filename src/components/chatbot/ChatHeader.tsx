b42e95d5621793244e04330513797ad483631307
vcky4 <okon.victor.u@gmail.com>
2025-04-27 14:46:14 +0100

    fix: change header location

--- src/components/chatbot/ChatHeader.tsx
+++ src/components/chatbot/ChatHeader.tsx
@@ -5,7 +5,7 @@ const ChatHeader: React.FC = () => {
     <div className="bg-[rgba(13,24,46,1)] flex w-full flex-col overflow-hidden text-[43px] text-[rgba(50,205,50,1)] font-extrabold whitespace-nowrap pb-3.5 px-[70px]">
       <div className="flex flex-col relative aspect-[1.513] gap-[15px] overflow-hidden py-[57px]">
         <img
-          src="/public/header.png"
+          src="/header.png"
           alt="Background pattern"
           className="absolute h-full w-full object-cover inset-0"
         />
