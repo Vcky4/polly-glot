15eabf6a76cc7496c47a54a038b9a215665bd64a
vcky4 <okon.victor.u@gmail.com>
2025-04-27 14:12:53 +0100

    feet: setup open ai hook

---
+++ src/hooks/useOpenAi.ts
@@ -0,0 +1,27 @@
+import OpenAI from "openai";
+
+const useOpenAi = () => {
+    const openai = new OpenAI({
+        apiKey: OPENAI_API_KEY,
+        dangerouslyAllowBrowser: true,
+    });
+    const getTranslation = async (prompt: string, language: string) => {
+        const completion = await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            store: true,
+            messages: [
+                { role: 'system', content: `You are a language translator. you only translate text to ${language}` },
+                { role: 'user', content: prompt }
+            ],
+            temperature: 0.5,
+        });
+        const response = completion.choices[0].message.content;
+        return response;
+
+    }
+    return { getTranslation };
+}
+
+
+export default useOpenAi;
+const OPENAI_API_KEY = ''
